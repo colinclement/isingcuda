e3f8dc203d6503994cab213d3330033bb6525031
Colin Clement <colin.clement@gmail.com>
2016-04-11 19:37:55 -0400

    drastically simplified structure, debugged

--- sampling.cu
+++ sampling.cu
@@ -12,82 +12,68 @@
 #define MAX(a, b) ((a > b) ? a : b)
 #endif
 
+#define PI_F 3.141592654f
+#define TPI_F 6.283185307f
+
 __device__
-float calculate_dE(const float *s_spins, const float trial, const int row, const int col,
-                   const float step, const int site, const int sharedsite){
+float calculate_dE(const float *s_spins, const float trial,
+                   const float step, const int sharedsite){
+        int tidy = threadIdx.y, tidx = threadIdx.x; 
+        
         float c = s_spins[sharedsite];
-        float u = s_spins[(row - 1) * blockDim.x + col];
-        float l = s_spins[row * blockDim.x + (col - 1)];
-        float r = s_spins[row * blockDim.x + (col + 1)];
-        float d = s_spins[(row + 1) * blockDim.x + col];
-        float E0 = -(cosf(c-u)+cosf(c-l)+cosf(c-r)+cosf(c-d));
-        float c1 = c + trial;
-        float E1 = -(cosf(c1-u)+cosf(c1-l)+cosf(c1-r)+cosf(c1-d));
+        float u = s_spins[(tidy - 1) * blockDim.x + tidx];
+        float l = s_spins[tidy * blockDim.x + (tidx - 1)];
+        float r = s_spins[tidy * blockDim.x + (tidx + 1)];
+        float d = s_spins[(tidy + 1) * blockDim.x + tidx];
+        
+        float E0 = -(cosf(c -u)+cosf(c -l)+cosf(c -r)+cosf(c -d));
+        float E1 = -(cosf(trial-u)+cosf(trial-l)+
+                     cosf(trial-r)+cosf(trial-d));
         return E1-E0;
     }
 
-
-__device__
-void chessBoardUpdate(float *s_spins, float *d_spins, float *d_random,
-                      float *d_random_step, const float T, const int site, 
-                      const int sharedsite, const float step){
-    
-    int row = threadIdx.y, col = threadIdx.x; 
-    //Load spins to shared memory
-    s_spins[sharedsite] = d_spins[site];
-    __syncthreads();//wait until spins are loaded
-    
-    if (row == 0 || col == 0 || row == blockDim.y-1 || col == blockDim.x-1)
-        return; //Edge site for shared memory filling
-    
-    int chess = (row%2 + col%2)%2;
-    
-    if (chess == 0){
-        float trial = s_spins[sharedsite] + (d_random_step[site]-0.5)*2*M_PI*step;
-        float dE = calculate_dE(s_spins, trial, row, col, 
-                                step, site, sharedsite);
-        if (exp(-dE/T) > d_random[site])
-            s_spins[sharedsite] = trial;
-    }
-    
-    __syncthreads();
-    if (chess == 1){
-        float trial = s_spins[sharedsite] + (d_random_step[site]-0.5)*2*M_PI*step;
-        float dE = calculate_dE(s_spins, trial, row, col, 
-                                step, site, sharedsite);
-        if (exp(-dE/T) > d_random[site])
-            s_spins[sharedsite] = trial;
-    } 
-    __syncthreads();
-
-    //Update spins
-    d_spins[site] = fmodf(s_spins[sharedsite], 2.f*M_PI);
-    __syncthreads();
-    
-    return;
-}
-
-
 __global__
-void isingSample(float *d_spins, float *d_random, float *d_random_step,
+void isingSample(float *d_spins, const float *d_random, const float *d_random_step, 
                  const float T, const int L, const float step){
+    
     int tidx = threadIdx.x, tidy = threadIdx.y;
     // bdim - 2 because we don't update boundary
     int x = (int)(tidx + blockIdx.x * (blockDim.x - 2)) - 1;
     int y = (int)(tidy + blockIdx.y * (blockDim.y - 2)) - 1;
     if (x > L || y > L)
-        return; //Prevents overlap with incommensurate blocks
+        return; //Limits to system + nearest neighbor
     
-    int col = MOD(x, L);
-    int row = MOD(y, L);
-    int site = row * L + col, sharedsite = tidy * blockDim.x + tidx;
-    extern __shared__ float s_spins[];//(blockDim+2)**2
+    int col = MOD(x, L), row = MOD(y, L);
+    int site = row*L + col, sharedsite = tidy * blockDim.x + tidx;
+    extern __shared__ float s_spins[];
 
-    chessBoardUpdate(s_spins, d_spins, d_random, d_random_step, T,
-                     site, sharedsite, step);
+    s_spins[sharedsite] = d_spins[site];
+    __syncthreads();//wait until spins are loaded
+   
+    int edge = tidx==0 || tidy==0 || tidy==blockDim.y-1 || tidx==blockDim.x-1;
+    if (edge || x == L || y == L)
+        return; //Edge sites don't get updated! 
+
+    int chess = (tidx%2 + tidy%2)%2;
     
+    if (chess == 0){
+        float trial = s_spins[sharedsite] + (d_random_step[site]-0.5f)*TPI_F*step;
+        float dE = calculate_dE(s_spins, trial, step, sharedsite);
+        if (dE < 0 || expf(-dE/T) > d_random[site])
+            s_spins[sharedsite] = trial;
+    }
+    __syncthreads();
+    if (chess == 1){
+        float trial = s_spins[sharedsite] + (d_random_step[site]-0.5f)*TPI_F*step;
+        float dE = calculate_dE(s_spins, trial, step, sharedsite);
+        if (dE < 0 || expf(-dE/T) > d_random[site])
+            s_spins[sharedsite] = trial;
+    } 
+    __syncthreads();
+
+    //Update spins
+    d_spins[site] = s_spins[sharedsite];
     return;
 }
-
 
 
