89ebc1e448f2361baccb8356ca5169f72b49224a
Colin Clement <colin.clement@gmail.com>
2016-04-11 19:37:32 -0400

    simplify streams to use default for sampling

--- main.cu
+++ main.cu
@@ -25,8 +25,8 @@
 
 //I actually use THREAD_X+2, THREAD_Y+2
 // (THREAD_X+2)*(THREAD_Y+2) < 1024
-#define THREAD_X 14 
-#define THREAD_Y 14
+#define THREAD_X 30 
+#define THREAD_Y 30
 
 int main(int argc, char **argv){
 
@@ -64,12 +64,13 @@ int main(int argc, char **argv){
     checkCudaErrors(curandCreateGenerator(&rng, CURAND_RNG_PSEUDO_DEFAULT));
     checkCudaErrors(curandSetPseudoRandomGeneratorSeed(rng, 920989ULL));
 
-    cudaStream_t cpyStream, rngStream, sampleStream;
+    cudaStream_t cpyStream, rngStream;
     checkCudaErrors(cudaStreamCreate(&cpyStream));
     checkCudaErrors(cudaStreamCreate(&rngStream));
-    checkCudaErrors(cudaStreamCreate(&sampleStream));
     
+    #ifndef DBUG
     checkCudaErrors(curandSetStream(rng, rngStream));
+    #endif
 
     float *h_spins = (float *)malloc(sizeof(float) * N);
     memset(h_spins, 0, sizeof(float) * N);
@@ -78,6 +79,7 @@ int main(int argc, char **argv){
     for (int i = 0; i < N; i++){
         h_spins[i] = 2*M_PI*(float)rand()/RAND_MAX;
     }
+
     float *d_spins;
     float *d_random, *d_random_step; 
     checkCudaErrors(cudaMalloc((void **)&d_spins, sizeof(float) * N));
@@ -87,36 +89,68 @@ int main(int argc, char **argv){
     checkCudaErrors(curandGenerateUniform(rng, d_random, 2*N));
     d_random_step = d_random + N;
 
+    // Define CUDA blocks and memory sizes
     int BLOCKS_X = ceil((float)L/(float)THREAD_X);
     int BLOCKS_Y = ceil((float)L/(float)THREAD_Y);
     int BLOCKMEM = sizeof(float) * (THREAD_X+2) * (THREAD_Y+2);
     dim3 blocks(BLOCKS_X, BLOCKS_Y);
     dim3 threads(THREAD_X+2, THREAD_Y+2); //include boundary
+
     #ifdef DBUG
+    float *h_random = (float *)malloc(sizeof(float)*2*N);
+    checkCudaErrors(cudaMemcpy(h_random, d_random, sizeof(float)*2*N,
+                               cudaMemcpyDeviceToHost));
+    FILE *fpdb = fopen("dbug.dat", "w");
+    for (int i=0; i < N; i++){
+        if (i%L==0)
+            fprintf(fpdb, "\n");
+        fprintf(fpdb, "%f\t", h_spins[i]);
+    }
+    for (int i=0; i < 2*N; i++){
+        if (i%L==0)
+            fprintf(fpdb, "\n");
+        fprintf(fpdb, "%f\t", h_random[i]);
+    }
+    fclose(fpdb);
     printf("Blocks(%d, %d)\n", BLOCKS_X, BLOCKS_Y); 
     printf("Threads(%d, %d)\n", THREAD_X+2, THREAD_Y+2);
     #endif
 
+    //Timing stuff
     cudaEvent_t start, stop;
     float time = 0.f;
     checkCudaErrors(cudaEventCreate(&start));
     checkCudaErrors(cudaEventCreate(&stop));
     checkCudaErrors(cudaEventRecord(start, 0));
 
+    //Burnin
     for (int t = 0; t < burnin; t++){
-        checkCudaErrors(cudaStreamSynchronize(rngStream));
+        //checkCudaErrors(cudaStreamSynchronize(rngStream));
         isingSample<<<blocks, threads, 
-                      BLOCKMEM, sampleStream>>>(d_spins, d_random, d_random_step, T, L, step);
+                      BLOCKMEM>>>(d_spins, d_random, d_random_step, T, L, step);
         checkCudaErrors(curandGenerateUniform(rng, d_random, 2*N));
         d_random_step = d_random + N;
     } 
-    
+    printf("Burn-in complete, sampling\n");
+
     FILE *fpSave = fopen(filename, "w");
 
     for (int t = 0; t < N_steps; t++){
+       
+        //checkCudaErrors(cudaStreamSynchronize(rngStream));
+        isingSample<<<blocks, threads, 
+                      BLOCKMEM>>>(d_spins, d_random, d_random_step, T, L, step);
+        checkCudaErrors(curandGenerateUniform(rng, d_random, 2*N));
+        d_random_step = d_random + N;
+        
         if (t % period == 0){
+            //Quit changing d_spins before copying
+            //checkCudaErrors(cudaDeviceSynchronize());
+            //checkCudaErrors(cudaStreamSynchronize(sampleStream));
             checkCudaErrors(cudaMemcpyAsync(h_spins, d_spins, sizeof(float)*N,
-                                             cudaMemcpyDeviceToHost, cpyStream));
+                                            cudaMemcpyDeviceToHost, cpyStream));
+            //checkCudaErrors(cudaMemcpy(h_spins, d_spins, sizeof(float)*N,
+            //                           cudaMemcpyDeviceToHost));
             for (int i=0; i < N; i++){
                 if (i%L==0)
                     fprintf(fpSave, "\n");
@@ -124,12 +158,7 @@ int main(int argc, char **argv){
             }
             fprintf(fpSave, "\n");
         }
-        
-        checkCudaErrors(cudaStreamSynchronize(rngStream));
-        isingSample<<<blocks, threads, 
-                      BLOCKMEM, sampleStream>>>(d_spins, d_random, d_random_step, T, L, step);
-        checkCudaErrors(curandGenerateUniform(rng, d_random, 2*N));
-        d_random_step = d_random + N;
+        //checkCudaErrors(cudaDeviceSynchronize());
         
     }
 
@@ -141,8 +170,10 @@ int main(int argc, char **argv){
     printf("Elapsed time: %f ms, %f ms / site updated\n", time, time/(float)(burnin+N_steps+N));
     
     checkCudaErrors(cudaStreamDestroy(cpyStream));
+    #ifndef DBUG
     checkCudaErrors(cudaStreamDestroy(rngStream));
-    checkCudaErrors(cudaStreamDestroy(sampleStream));
+    #endif
+    //checkCudaErrors(cudaStreamDestroy(sampleStream));
     checkCudaErrors(curandDestroyGenerator(rng));
 
     cudaFree(d_spins);
