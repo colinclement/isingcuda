073b329e8a24a766998415865f320232839f8f93
Colin Clement <colin.clement@gmail.com>
2016-04-11 19:38:07 -0400

    only expose sampling to main

--- sampling.h
+++ sampling.h
@@ -2,12 +2,7 @@
 #define __SAMPLING_H__
 
 __global__
-void isingSample(float *d_spins, float *d_random, float *d_random_step,
+void isingSample(float *d_spins, const float *d_random, const float *d_random_step,
                  const float T, const int L, const float step);
-
-__device__
-void chessBoardUpdate(float *s_spins, float *d_spins, float *d_random,
-                      float *d_random_step, const float T, const int site, 
-                      const int sharedsite, const float step );
 
 #endif
